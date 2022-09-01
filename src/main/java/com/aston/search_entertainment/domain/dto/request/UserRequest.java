215f50bd7838390ed5a9659c31ab3d25bb2eddd0
ZubrovIV <zubroviv@gmail.com>
2022-09-02 00:59:16 +0300

    create UserEntity some fixed

--- src/main/java/com/aston/search_entertainment/domain/dto/request/UserRequest.java
+++ src/main/java/com/aston/search_entertainment/domain/dto/request/UserRequest.java
@@ -5,13 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserRequest {
     private String email;
+    private String password;
     private String firstName;
     private String lastName;
+    private LocalDate created;
     private String role;
 }
--- src/main/java/com/aston/search_entertainment/domain/dto/request/UserRequestUpdate.java
+++ src/main/java/com/aston/search_entertainment/domain/dto/request/UserRequestUpdate.java
@@ -4,12 +4,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class UserRequestUpdate {
     private String email;
+    private String password;
     private String firstName;
     private String lastName;
+    private LocalDate created;
     private String role;
 }
