71f277322f57819c8e380e133150545188cdab98
moryi <mory.ivan@gmail.com>
2022-09-03 13:44:02 +0300

    Remove auto import

--- src/main/java/com/aston/search_entertainment/domain/entity/CompanyEntity.java
+++ src/main/java/com/aston/search_entertainment/domain/entity/CompanyEntity.java
@@ -1,10 +1,23 @@
 package com.aston.search_entertainment.domain.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 
 @Getter
 @Setter
--- src/main/java/com/aston/search_entertainment/domain/entity/UserEntity.java
+++ src/main/java/com/aston/search_entertainment/domain/entity/UserEntity.java
@@ -8,7 +8,14 @@ import lombok.ToString;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import java.time.LocalDate;
 import java.util.List;
