e4ddb6ca02fd4965dadaef4d75603d41ff2d55f1
arctg07 <arctg07@gmail.com>
2022-09-03 21:34:55 +0300

    UserEntity to User

--- src/main/java/com/aston/search_entertainment/domain/dto/request/CommentResponse.java
+++ src/main/java/com/aston/search_entertainment/domain/dto/request/CommentResponse.java
@@ -1,5 +1,14 @@
 package com.aston.search_entertainment.domain.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CommentResponse {
 
     private String text;
