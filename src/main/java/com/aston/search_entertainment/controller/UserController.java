46478edf3156ee4fecafbfdff6aa5b35bbece979
ZubrovIV <zubroviv@gmail.com>
2022-09-09 21:51:17 +0300

    fixed

--- src/main/java/com/aston/search_entertainment/controller/UserController.java
+++ src/main/java/com/aston/search_entertainment/controller/UserController.java
@@ -65,7 +65,6 @@ public class UserController {
         }
         return new ResponseEntity<>(result, OK);
     }
-
     @ApiOperation(value = "Создание нового пользователя")
     @PostMapping()
     public UserResponse createUser(@RequestBody UserRequest userRequest) {
