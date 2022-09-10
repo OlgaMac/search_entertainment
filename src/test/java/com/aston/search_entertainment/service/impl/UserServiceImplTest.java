b2ef6557edb5cec3b32670573592d46fe954cc4f
ZubrovIV <zubroviv@gmail.com>
2022-09-10 15:17:54 +0300

    user test first commit

---
+++ src/test/java/com/aston/search_entertainment/service/impl/UserServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.aston.search_entertainment.service.impl;
+
+import com.aston.search_entertainment.domain.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+    @Autowired
+    private UserServiceImpl userService;
+    @Autowired
+    private User user;
+
+}
