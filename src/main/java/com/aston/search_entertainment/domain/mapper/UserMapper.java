c358014b0c905f966efbd9d52243f3a9f0e9e988
OlgaMac <oljamackeewa@gmail.com>
2022-09-13 00:27:47 +0300

    fix

--- src/main/java/com/aston/search_entertainment/domain/mapper/UserMapper.java
+++ src/main/java/com/aston/search_entertainment/domain/mapper/UserMapper.java
@@ -6,7 +6,6 @@ import com.aston.search_entertainment.domain.dto.request.UserRequestUpdate;
 import com.aston.search_entertainment.domain.dto.response.UserResponse;
 import com.aston.search_entertainment.domain.entity.UserEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 
@@ -15,8 +14,6 @@ public interface UserMapper {
 
     UserResponse toResponse(UserEntity userEntity);
     UserEntity fromRequest(UserRequest userRequest);
-    @Mapping(target = "role", ignore = true)
-    @Mapping(target = "password", ignore = true)
     UserEntity fromRequestUpdate(UserRequestUpdate userRequestUpdate);
 
     UserEntity fromRegistrationRequest(RegistrationRequest request);
