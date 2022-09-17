faeb8b52bb88edbcae41c8ce79e1b204749375bc
OlgaMac <oljamackeewa@gmail.com>
2022-09-17 11:31:51 +0300

    Email validation

--- src/main/java/com/aston/search_entertainment/securety/controller/AuthenticationController.java
+++ src/main/java/com/aston/search_entertainment/securety/controller/AuthenticationController.java
@@ -15,6 +15,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,6 +39,7 @@ import static org.springframework.http.HttpStatus.FORBIDDEN;
 @RestController
 @RequestMapping(MAIN_URL + VERSION + AUTH_URL)
 @ApiOperation(value = "Аутентификация", notes = "Работа с аутентификацией")
+@Validated
 public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
@@ -44,12 +48,14 @@ public class AuthenticationController {
     @ApiOperation(value = "", authorizations = { @Authorization(value="JWT") })
     @ApiResponses(value = {@ApiResponse( code = 200, message = "Аутентификация пройдена успешно"),
     @ApiResponse(code = 400, message = "Внутренняя ошибка сервера"),
-    @ApiResponse(code = 403, message = "Неверно введен емейл или пароль")})
+    @ApiResponse(code = 403, message = "Неверно введен емейл или пароль"),
+    @ApiResponse(code = 402, message = "Неверно введен емейл")})
     @PostMapping(LOGIN_URL)
-    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest request) {
+    @Validated
+    public ResponseEntity<?> authenticate(@Valid @RequestBody AuthenticationRequest request) {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-            UserEntity userEntity = userDetailService.getUserByEmail(request.getEmail());
+            UserEntity userEntity = userDetailService.getUserByEmail( request.getEmail());
             String token = jwtTokenProvider.createToken(request.getEmail(), userEntity.getRole().name());
             Map<Object, Object> response = new HashMap<>();
             response.put("email", request.getEmail());
@@ -57,6 +63,8 @@ public class AuthenticationController {
             return ResponseEntity.ok(response);
         } catch (AuthenticationException e) {
             return new ResponseEntity<>("Invalid email/password combination", FORBIDDEN);
+        } catch (ConstraintViolationException ex){
+            return new ResponseEntity<>("Invalid email", FORBIDDEN);
         }
     }
 
---
+++ src/main/java/com/aston/search_entertainment/validation/validator/Regions.java
@@ -0,0 +1,14 @@
+package com.aston.search_entertainment.validation.validator;
+
+/**
+ * enum Regions
+ *
+ * @author damir.iusupov
+ * @since 2022-06-16
+ */
+public enum Regions {
+    ru,
+    com,
+    by,
+    kz
+}
